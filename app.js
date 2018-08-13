61b9e53ba185c6544ebf60c6fc17ac6b3346e0fc
Vincent Besseau <vbesseau44@gmail.com>
2018-08-13 08:59:25 +0200

    change dependance

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if ( typeof sails !== 'undefined' && sails ) {
     return '';
 }
 const pathToStoreFiles = './store/Broadlink/jsonCommands'
-var Broadlink = require("broadlink-js-smth");
+var Broadlink = require("broadlink-js");
 var uuidv4 = require('uuid/v4');
 var jsonStore = require('json-fs-store')(pathToStoreFiles);
 var fs = require('fs');
@@ -22,90 +22,93 @@ var gladysMqttAdapter = require('gladys-mqtt-adapter')({
 
 
 
-var blink = new Broadlink();
-blink.discover(null,["b4:43:0d:70:6f:a0"]);
+var blink = new Broadlink.Broadlink();
 
-blink.on("deviceReady",function(dev){
-    console.log("typeof mac >>> "+typeof dev.mac);
-    console.log(">>>>>>>>>>>>>>>find dev ip=" + dev.host.address +" type="+dev.type);
-    // if(dev.set_power)dev.set_power(true);
-    if(dev.type == 'RM2'){
+blink.on('listen', function(res) {
+    console.log('listening', res);
+})
 
-        gladysMqttAdapter.on('message-notify', function(data) {
-			console.log(data)
-			switch (data._type) {
-				case 'learning':
-					console.log("enterLearning mode");
-		            dev.enterLearning();
-		            var checkLearning = setInterval(() => {
-		                dev.checkData();
-		                // dev.enterLearning();
-		            }, 1000)
+blink.on('discover', function(res) {
+    console.log('discover', res);
+    console.log(res.module)
+    if(res.module === 'rm2'){
+    	var dev = new Broadlink.BroadlinkDeviceRM2(res);
+    	dev.on('ready', function(res) {
+	        gladysMqttAdapter.on('message-notify', function(data) {
+				console.log(data)
+				switch (data._type) {
+					case 'learning':
+						console.log("enterLearning mode");
+						dev.learnCode(function(res,object) {
+				            console.log(object);
+				            var newId = uuidv4()
+	            
+				            while (fs.existsSync(pathToStoreFiles+'/'+newId+'.json')) {
+							    newId = uuidv4()
+							}
+				            
+				            var newJson = {
+				            	id:newId,
+				            	command: object.toString('hex')
+				            }
+				            jsonStore.add(newJson, function(err) {
+				            	if (err) throw err;
+				            })
 
-		            dev.on("rawData",(learn) =>{
-		                clearInterval(checkLearning);
-		                var newId = uuidv4()
-		                
-		                while (fs.existsSync(pathToStoreFiles+'/'+newId+'.json')) {
-						    newId = uuidv4()
-						}
-		                
-		                var newJson = {
-		                	id:newId,
-		                	command: learn.toString('hex')
-		                }
-		                jsonStore.add(newJson, function(err) {
-		                	if (err) throw err;
-		                })
-
-		                var uri = config.baseUri + "/devicetype?token="+config.token
-		                var json = {
-								    "name": data._name,
-								    "type": "binary",
-								    "identifier":newId,
-								    "min":0,
-								    "max":1,
-								    "sensor":false,
-								    "device":data._deviceId
-								  }
-						var options = {
-						    url: uri,
-						    json: true,
-						    method: 'POST',
-						    headers: {
-							        	"content-type": "application/json",
-							        },
-						    json: json
-						}
-						request(options, function (error, response, body) {
-						    if (!error && response.statusCode == 200) {
-						        // Print out the response body
-						        console.log('DeviceType Create with success')
-						        learn = null
-						    }
-						    else {
-						    	console.log('Error HTTP '+response.statusCode)
-						    }
-						})
-		                console.log("hex:"+newJson.command);
-		                
-		            })
-					break;
-				case 'sendData':
-					var uuid = data._uuid
-		            console.log(uuid)
-		            jsonStore.load(uuid, function(err, object){
-						if(err) throw err;
-						var convertHexToBuffer = new Buffer(object.command, "hex")
-		            	dev.sendData(convertHexToBuffer)
-					});
-			        break;
-		        default:
-		            console.log('Message non reconnu');
-			}
-		})
-        
+				            var uri = config.baseUri + "/devicetype?token="+config.token
+				            var json = {
+									    "name": data._name,
+									    "type": "binary",
+									    "identifier":newId,
+									    "min":0,
+									    "max":1,
+									    "sensor":false,
+									    "device":data._deviceId
+									  }
+							var options = {
+							    url: uri,
+							    json: true,
+							    method: 'POST',
+							    headers: {
+								        	"content-type": "application/json",
+								        },
+							    json: json
+							}
+							request(options, function (error, response, body) {
+							    if (!error && response.statusCode == 200) {
+							        // Print out the response body
+							        console.log('DeviceType Create with success')
+							        learn = null
+							    }
+							    else {
+							    	console.log('Error HTTP '+response.statusCode)
+							    }
+							})
+				            console.log("hex:"+newJson.command);
+				        });
+						break;
+					case 'sendData':
+						var uuid = data._uuid
+			            console.log(uuid)
+			            jsonStore.load(uuid, function(err, object){
+							if(err) console.log(err);
+							var convertHexToBuffer = new Buffer(object.command, "hex")
+			            	dev.sendData(convertHexToBuffer)
+						});
+				        break;
+			        default:
+			            console.log('Message non reconnu');
+				}
+			})
+	        dev.on("rawData",(learn) =>{
+	            clearInterval(checkLearning);
+	            dev.cancelLearn();
+	            
+	            
+	        })
+	    });
         
     }
-    blink.discover(null,[]);
 });
+
+blink.discover();
