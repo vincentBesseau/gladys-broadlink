97a3373210b9662e4ceb19ed65bad6cdb726bab2
Vincent Besseau <vbesseau44@gmail.com>
2018-08-23 21:09:19 +0200

    Delete device when module will be uninstall

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ module.exports = function (sails) {
 
     var exec = require('./lib/broadlink.exec.js');
     var BroadlinkController = require('./controller/broadlinkController.js');
+    var uninstall = require = require('./lib/uninstall.js')
 
     return {
         exec,
+        uninstall,
         routes: {
             before: {
                 'post /broadlink/learning': (req, res, next) => sails.hooks.policies.middleware.checktoken(req, res, next)
