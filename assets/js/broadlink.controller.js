bfc7bbee996fa494d89bd46c6f756c6e47fc5769
piznel <piznel@hotmail.com>
2018-11-02 18:38:49 +0100

    change view

--- assets/js/broadlink.controller.js
+++ assets/js/broadlink.controller.js
@@ -1,44 +1,62 @@
- (function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular
-        .module('gladys')
-        .controller('BroadlinkCtrl', BroadlinkCtrl);
+  angular
+    .module('gladys')
+    .controller('BroadlinkCtrl', BroadlinkCtrl);
 
-    BroadlinkCtrl.$inject = ['broadlinkService', 'deviceService', '$scope'];
+  BroadlinkCtrl.$inject = ['broadlinkService', 'deviceService', '$scope'];
 
-    function BroadlinkCtrl(broadlinkService, deviceService, $scope) {
-        /* jshint validthis: true */
-        var vm = this;
-        vm._name = null;
-        vm.LearningMode = LearningMode;
-        vm.RM2devices = [];
+  function BroadlinkCtrl(broadlinkService, deviceService, $scope) {
+    /* jshint validthis: true */
+    var vm = this;
+    //vm._name = null;
+    vm.LearningMode = LearningMode;
+    vm.RM2devices = [];
+    vm.RM2deviceType = [];
+    vm.getRM2DeviceType = getRM2DeviceType;
+    vm.selectedDevice = '';
 
-        getRM2Device();
+    getRM2Device();
 
-        function getRM2Device() {
-            return deviceService.get()
-            .then(function(devices) {
-                devices.data.forEach(function(device) {
-                    if(device.service === 'broadlink' && device.protocol === 'RM2') {
-                        vm.RM2devices.push(device);                        
-                    }
-                })
-                return vm.RM2devices;
-            })
-        }
-
-        function LearningMode(id,name) {
-            return broadlinkService.learningMode({'deviceId':id, '_name':name})
-                .then(function(result){
-                    if(result.status === 200){
-                        console.log('LearningMode ok')
-                    }else{
-                        console.log('LearningMode ko')
-                    }
-                })
-        }
-        
-        
+    function getRM2Device() {
+      return deviceService.get()
+        .then(function(devices) {
+          devices.data.forEach(function(device) {
+            if (device.service === 'broadlink' && device.protocol === 'RM2') {
+              vm.RM2devices.push(device);
+            }
+          })
+          return vm.RM2devices;
+        })
     }
+
+    function LearningMode(id, name) {
+      return broadlinkService.learningMode({ 'deviceId': vm.selectedDevice.id, '_name': name })
+        .then(function(result) {
+          if (result.status === 200) {
+            console.log('LearningMode ok')
+            getRM2DeviceType(vm.selectedDevice)
+          } else {
+            console.log('LearningMode ko')
+          }
+        })
+    }
+
+    function getRM2DeviceType(device) {
+      return deviceService.getDeviceTypesDevice(device.id)
+        .then(function(data) {
+          vm.RM2deviceType = [];
+          if (data.data.length > 0) {
+            data.data.forEach(function(deviceType) {
+              vm.RM2deviceType.push((deviceType));
+            })
+          }
+          console.log('RM2deviceType', vm.RM2deviceType)
+          return vm.RM2deviceType;
+        })
+    }
+
+
+  }
 })();
